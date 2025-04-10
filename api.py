0bf740cbab2453d239346c5127ae9c23a31561bb
Aakash <ahanda205@gmail.com>
2025-04-10 23:35:55 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
 from bson import ObjectId
-#46
+#47
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/your_database_name"
 mongo = PyMongo(app)
